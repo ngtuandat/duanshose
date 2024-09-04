f5930638fda5af142dffb824946a5931d9225012
ngtuandat <ngtuandat.15@gmail.com>
2024-09-04 17:53:49 +0700

    thongke vs detail vanlai

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -22,6 +22,9 @@ const columnProduct = [
 const Dashboard = ({ loading }: { loading: Boolean }) => {
   const currentDate = new Date();
   const [data, setData] = useState([]);
+  const [sellWell, setSellWell] = useState<any>([]);
+  console.log(sellWell, "sellWell");
+
   const currentDay = currentDate.toISOString().slice(0, 10); // YYYY-MM-DD
 
   const [countProd, setCountProd] = useState(0);
@@ -30,10 +33,11 @@ const Dashboard = ({ loading }: { loading: Boolean }) => {
   const [totalEstimatedPrice, setTotalEstimatedPrice] = useState(0);
   const [selling, setSelling] = useState<any[]>([]);
   const [selectedDate, setSelectedDate] = useState<string | null>(currentDay);
+  console.log(sellWell, "sellWell");
+
   const [selectedEndDate, setSelectedEndDate] = useState<string | null>(
     currentDay
   );
-
   const fetchAllPurchase = async (
     date: string | null,
     endDate: string | null
@@ -42,6 +46,13 @@ const Dashboard = ({ loading }: { loading: Boolean }) => {
       const res = await getPurchaseAll(); // Gọi API để lấy tất cả dữ liệu từ backend
       const allProducts = res.data.result;
       setData(allProducts);
+      setSelling(res.data.result);
+      const deliveredProductsSellWell = allProducts.filter(
+        (product: PurchaseProps) => product.status === "delivered"
+      );
+
+      setSellWell(deliveredProductsSellWell);
+
       console.log(allProducts, "allProducts");
 
       // Xác định ngày bắt đầu và kết thúc để lọc dữ liệu
@@ -109,8 +120,11 @@ const Dashboard = ({ loading }: { loading: Boolean }) => {
       // Tạo map để tính tổng doanh thu của từng sản phẩm đã bán
       const productMap = new Map<string, any>();
 
-      deliveredProducts.forEach((item: PurchaseProps) => {
+      deliveredProductsSellWell.forEach((item: PurchaseProps) => {
         const key = `${item.nameProd}-${item.sizeProd}-${item.colorProd}`;
+        {
+          console.log(productMap, "productMap");
+        }
         if (productMap.has(key)) {
           const existingProduct = productMap.get(key);
           existingProduct.quantitySold += item.quantityProd;
@@ -127,15 +141,129 @@ const Dashboard = ({ loading }: { loading: Boolean }) => {
         }
       });
 
-      // Sắp xếp sản phẩm theo số lượng bán được giảm dần
-      const soldProducts = Array.from(productMap.values()).sort(
-        (a, b) => b.quantitySold - a.quantitySold
-      );
-      setSelling(soldProducts);
+      // Sắp xếp sản phẩm theo số lượng bán được giảm dần, nếu số lượng bằng nhau thì sắp xếp theo doanh thu giảm dần
+      const soldProducts = Array.from(productMap.values()).sort((a, b) => {
+        if (b.quantitySold === a.quantitySold) {
+          return b.totalRevenue - a.totalRevenue;
+        }
+        return b.quantitySold - a.quantitySold;
+      });
+
+      setSellWell(soldProducts);
     } catch (error) {
       console.log(error);
     }
   };
+
+  // const fetchAllPurchase = async (
+  //   date: string | null,
+  //   endDate: string | null
+  // ) => {
+  //   try {
+  //     const res = await getPurchaseAll(); // Gọi API để lấy tất cả dữ liệu từ backend
+  //     const allProducts = res.data.result;
+  //     setData(allProducts);
+  //     setSelling(allProducts);
+  //     const deliveredProductsSellWell = allProducts.filter(
+  //       (product: PurchaseProps) => product.status === "delivered"
+  //     );
+
+  //     setSellWell(deliveredProductsSellWell);
+
+  //     console.log(allProducts, "allProducts");
+
+  //     // Xác định ngày bắt đầu và kết thúc để lọc dữ liệu
+  //     const startDate = date ? new Date(date) : new Date("1900-01-01"); // Ngày bắt đầu
+  //     const endDateParsed = endDate
+  //       ? new Date(endDate + "T23:59:59")
+  //       : new Date(); // Ngày kết thúc, bao gồm toàn bộ ngày
+
+  //     // Lọc sản phẩm trong ngày và tháng được chọn
+  //     const filteredProducts = allProducts.filter((product: PurchaseProps) => {
+  //       const productDate = new Date(product.createdAt);
+
+  //       // Lọc theo ngày bắt đầu và ngày kết thúc
+  //       const isWithinDateRange =
+  //         productDate >= startDate && productDate <= endDateParsed;
+
+  //       return isWithinDateRange;
+  //     });
+
+  //     // Loại bỏ các đơn hàng ở trạng thái cancelled và returns
+  //     const validProducts = filteredProducts.filter(
+  //       (product: PurchaseProps) =>
+  //         product.status !== "cancelled" && product.status !== "returns"
+  //     );
+
+  //     const deliveredProducts = validProducts.filter(
+  //       (product: PurchaseProps) => product.status === "delivered"
+  //     );
+
+  //     const nonDeliveredProducts = validProducts.filter(
+  //       (product: PurchaseProps) => product.status !== "delivered"
+  //     );
+
+  //     // Tính toán số lượng sản phẩm và doanh thu thực
+  //     setCountProd(
+  //       deliveredProducts.reduce(
+  //         (acc: number, curr: PurchaseProps) => acc + curr.quantityProd,
+  //         0
+  //       )
+  //     );
+
+  //     setTemporaryQuantity(
+  //       nonDeliveredProducts.reduce(
+  //         (acc: number, curr: PurchaseProps) => acc + curr.quantityProd,
+  //         0
+  //       )
+  //     );
+
+  //     const totalEstimatedRevenue = nonDeliveredProducts.reduce(
+  //       (acc: number, cur: PurchaseProps) => {
+  //         return acc + cur.priceProd * cur.quantityProd;
+  //       },
+  //       0
+  //     );
+  //     setTotalEstimatedPrice(totalEstimatedRevenue);
+
+  //     const totalRevenue = deliveredProducts.reduce(
+  //       (acc: number, cur: PurchaseProps) => {
+  //         return acc + cur.priceProd * cur.quantityProd;
+  //       },
+  //       0
+  //     );
+  //     setTotalPrice(totalRevenue);
+
+  //     // Tạo map để tính tổng doanh thu của từng sản phẩm đã bán
+  //     const productMap = new Map<string, any>();
+
+  //     deliveredProducts.forEach((item: PurchaseProps) => {
+  //       const key = `${item.nameProd}-${item.sizeProd}-${item.colorProd}`;
+  //       if (productMap.has(key)) {
+  //         const existingProduct = productMap.get(key);
+  //         existingProduct.quantitySold += item.quantityProd;
+  //         existingProduct.totalRevenue += item.priceProd * item.quantityProd;
+  //       } else {
+  //         productMap.set(key, {
+  //           productName: item.nameProd,
+  //           size: item.sizeProd,
+  //           color: item.colorProd,
+  //           quantitySold: item.quantityProd,
+  //           imagesProd: item.imageProd,
+  //           totalRevenue: item.priceProd * item.quantityProd,
+  //         });
+  //       }
+  //     });
+
+  //     // Sắp xếp sản phẩm theo số lượng bán được giảm dần
+  //     const soldProducts = Array.from(productMap.values()).sort(
+  //       (a, b) => b.quantitySold - a.quantitySold
+  //     );
+  //     setSelling(soldProducts);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
   useEffect(() => {
     fetchAllPurchase(selectedDate, selectedEndDate); // Gọi hàm fetchAllPurchase khi selectedDate hoặc selectedEndDate thay đổi
@@ -150,6 +278,19 @@ const Dashboard = ({ loading }: { loading: Boolean }) => {
   const handleEndDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSelectedEndDate(event.target.value); // Cập nhật giá trị selectedEndDate khi người dùng thay đổi ngày kết thúc
   };
+
+  const groupedSellWell = sellWell.reduce((acc: any, item: any) => {
+    const foundIndex = acc.findIndex(
+      (i: any) => i.productName === item.productName
+    );
+    if (foundIndex >= 0) {
+      acc[foundIndex].quantitySold += item.quantitySold;
+      acc[foundIndex].totalRevenue += item.totalRevenue;
+    } else {
+      acc.push({ ...item });
+    }
+    return acc;
+  }, []);
 
   return (
     <div className="text-white">
@@ -216,11 +357,12 @@ const Dashboard = ({ loading }: { loading: Boolean }) => {
       <div className="text-lg flex justify-center py-6 font-bold">
         Sản phẩm bán Chạy
       </div>
+
       <div className="overflow-x-auto">
         <div className="flex flex-col">
           <div className="py-2 -mx-4 sm:-mx-6 lg:-mx-8 ">
             <div className="inline-block min-w-full align-middle">
-              <div className="relative overflow-hidden shadow-lg rounded-lg ring-1 ring-black ring-opacity-10 rounded-lg">
+              <div className="relative overflow-hidden shadow-lg rounded-lg ring-1 ring-black ring-opacity-10 ">
                 <table className="table-auto divide-y divide-gray-300 min-w-full">
                   <thead className="bg-gray-800 text-gray-200">
                     <tr>
@@ -236,7 +378,7 @@ const Dashboard = ({ loading }: { loading: Boolean }) => {
                     </tr>
                   </thead>
                   <tbody className="divide-y divide-gray-700 bg-gray-900">
-                    {selling.map((item, index) => (
+                    {groupedSellWell.map((item: any, index: any) => (
                       <tr
                         key={index}
                         className="hover:bg-gray-800 transition-colors duration-200"
@@ -267,7 +409,7 @@ const Dashboard = ({ loading }: { loading: Boolean }) => {
                         </td>
 
                         <td className="whitespace-nowrap text-center px-4 py-4 text-sm text-gray-300">
-                          {item.totalRevenue}
+                          {item.totalRevenue?.toLocaleString("vi")} đ
                         </td>
                       </tr>
                     ))}
